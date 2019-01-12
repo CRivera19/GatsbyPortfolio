87baff390ab1f183fb1d67c90a575f024442ef2b
Chris Rivera <Ctrivera19@gmail.com>
2019-01-11 22:48:28 -0800

    Update work.js

--- src/components/work.js
+++ src/components/work.js
@@ -1,3 +1,12 @@
-    <h2>Employment</h2>
-    <p>Currently looking for remote work.</p>
-    <p><mark>Relocated in AZ,US</mark></p>
+import React from 'react'
+ 
+const Work = () => (
+      <section style={{}}>
+        <h2 style={{ paddingTop: `0.3em`, marginBottom:`0`}}>
+          Employment
+        </h2>
+        <p><mark>Relocated in AZ,US</mark></p>
+      </section>
+)
+ 
+export default Work
