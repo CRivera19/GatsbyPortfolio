5e60113b48ce88c0fa9e2bb9666f161ac2d9b562
Ctrivera19 <ctrivera19@gmail.com>
2019-01-12 16:05:34 -0800

    changed border photo

--- src/components/about.js
+++ src/components/about.js
@@ -12,7 +12,7 @@ const About = () => (
         <h1 style={{ paddingTop: `0.3em`, marginBottom:`0`, alignSelf: `end`}}>
           About
         </h1>
-        <div style={{maxWidth:`150px`, gridColumnStart:`2`, gridColumnEnd:`4`,gridRowStart:`1`, gridRowEnd:`2`, }}>
+        <div style={{maxWidth:`150px`, gridColumnStart:`2`, gridColumnEnd:`4`,gridRowStart:`1`, gridRowEnd:`2`, border:`5px solid crimson`, borderRadius:`100%` }}>
         <Image />
         </div>
         <p style={{ gridRowStart:`2`, gridRowEnd:`4`, gridColumnStart:`1`, gridColumnEnd:`4` }}>My name is <mark><b>Christopher Rivera</b></mark> and I am <mark><b>Web Developer</b></mark> . I study Front-End and React. I am currently studying OOP, ES6, and Node.js to become a Full Stack developer. </p>
